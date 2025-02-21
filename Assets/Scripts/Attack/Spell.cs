f04e439e281bd0b8a0edaee57c3f3797c21ceb48
bruhenri12 <bruhenri12@hotmail.com>
2025-02-21 19:36:13 -0300

    Add spell fields and apply force  method

--- Assets/Scripts/Attack/Spell.cs
+++ Assets/Scripts/Attack/Spell.cs
@@ -4,6 +4,23 @@ namespace Brawlley.Attacks
 {
     public class Spell : Attack
     {
-        
+        #region Spell Resources
+        [Header("Spell Resources")]
+        [SerializeField] Rigidbody2D spellRigidbody;
+        #endregion
+
+        #region Spell Data
+        [Header("Spell Data")]
+        [SerializeField] float speed = 1f;
+        public float Speed { get => speed; set => speed = value; }
+        #endregion
+
+        #region Spell Methods
+        public void ApplyForce(Vector2 direction)
+        {
+            if (spellRigidbody == null)
+                spellRigidbody.AddForce(direction * speed, ForceMode2D.Impulse);
+        }
+        #endregion
     }
 }
