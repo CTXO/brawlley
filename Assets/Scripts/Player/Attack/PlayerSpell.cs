b8f0d72286509f4fa68af3b1a1266a787bc9855b
bruhenri12 <bruhenri12@hotmail.com>
2025-02-21 22:22:17 -0300

    Adding PlayerSpell fields

--- Assets/Scripts/Player/Attack/PlayerSpell.cs
+++ Assets/Scripts/Player/Attack/PlayerSpell.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
 
-namespace Brawlley
+namespace Brawlley.Player
 {
     public class PlayerSpell : PlayerAttack
     {
-        
+        #region Player Spell Resources
+        [Header("Player Spell Resources")]
+        public GameObject spellPrefab;
+        public Transform spellSpawnPoint;
+        #endregion
+
+        #region Player Spell Data
+        [Header("Player Spell Data")]
+        [SerializeField] float castTime = 0.5f;
+        public float CastTime { get => castTime; set => castTime = value; }
+        [SerializeField] float travelSpeed = 1f;
+        public float TravelSpeed { get => travelSpeed; set => travelSpeed = value; }
+        [SerializeField] float duration = 2f;
+        public float Duration { get => duration; set => duration = value; }
+        #endregion
     }
 }
