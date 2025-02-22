07fef6ce4382e93fb85f61a977f56ad7df64de2b
bruhenri12 <bruhenri12@hotmail.com>
2025-02-21 22:22:53 -0300

    Adding PlayerSpell Attack method and examples

--- Assets/Scripts/Player/Attack/PlayerSpell.cs
+++ Assets/Scripts/Player/Attack/PlayerSpell.cs
@@ -1,4 +1,6 @@
+using Brawlley.Attacks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Brawlley.Player
 {
@@ -18,6 +20,36 @@ namespace Brawlley.Player
         public float TravelSpeed { get => travelSpeed; set => travelSpeed = value; }
         [SerializeField] float duration = 2f;
         public float Duration { get => duration; set => duration = value; }
+        #endregion
+
+        #region MonoBehaviour Lifecycle Methods
+        protected override void Awake()
+        {
+            base.Awake();
+            GameInputs.Attack.Spell.performed += OnAttack;
+        }
+        #endregion
+
+        #region Player Spell Methods
+        public override void OnAttack(InputAction.CallbackContext context)
+        {
+            if (status == AttackStatus.Ready)
+            {
+                Vector2 attackDirection = GameInputs.Player.Movement.ReadValue<Vector2>();
+                Debug.Log($"Attack Direction: {attackDirection}");
+                GameObject spellObject = Instantiate(spellPrefab, spellSpawnPoint.position, Quaternion.identity);
+
+                Spell spell = spellObject.GetComponent<Spell>();
+                spell.Damage = Damage;
+                spell.KnockbackForce = KnockbackForce;
+                spell.IgnoreTeam = player.Team;
+                spell.Speed = travelSpeed;
+                spell.ApplyForce(attackDirection);
+                spell.StartDestroyAfterDuration();
+
+                StartCooldown();
+            }
+        }
         #endregion
     }
 }
