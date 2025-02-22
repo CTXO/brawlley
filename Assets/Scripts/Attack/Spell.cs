38a1152672f834cfbf5066b9b2ecc02e9dcc858b
bruhenri12 <bruhenri12@hotmail.com>
2025-02-21 22:17:17 -0300

    Add Spell class destruction methods

--- Assets/Scripts/Attack/Spell.cs
+++ Assets/Scripts/Attack/Spell.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Brawlley.Attacks
@@ -13,13 +14,36 @@ namespace Brawlley.Attacks
         [Header("Spell Data")]
         [SerializeField] float speed = 1f;
         public float Speed { get => speed; set => speed = value; }
+
+        [SerializeField] float duration = 2f;
+        public float Duration { get => duration; set => duration = value; }
         #endregion
 
         #region Spell Methods
         public void ApplyForce(Vector2 direction)
         {
             if (spellRigidbody == null)
-                spellRigidbody.AddForce(direction * speed, ForceMode2D.Impulse);
+                spellRigidbody.AddForce(new Vector2(direction.x, direction.y) * speed, ForceMode2D.Force);
+        }
+
+        public void OnTagCollisionDestroy(string tag)
+        {
+            if (currentCollision != null && currentCollision.CompareTag(tag))
+                Destroy(gameObject);
+        }
+
+        IEnumerator DestroyAfterDuration()
+        {
+            yield return new WaitForSeconds(duration);
+            Destroy(gameObject);
+        }
+
+        public void StartDestroyAfterDuration() => StartCoroutine(DestroyAfterDuration());
+
+        public void ResetDestroyAfterDuration()
+        {
+            StopCoroutine(DestroyAfterDuration());
+            StartCoroutine(DestroyAfterDuration());
         }
         #endregion
     }
