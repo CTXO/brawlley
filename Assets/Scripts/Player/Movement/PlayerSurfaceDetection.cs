79c95a01db27d8aad5f3df37c678fdc69065d47c
Artur Leal <albs@cin.ufpe.br>
2025-02-24 22:37:42 -0300

    Optimized Surface Detection

--- Assets/Scripts/Player/Movement/PlayerSurfaceDetection.cs
+++ Assets/Scripts/Player/Movement/PlayerSurfaceDetection.cs
@@ -5,6 +5,7 @@ public class PlayerSurfaceDetection : MonoBehaviour
     
     private bool onGround;
     private bool onWall;
+    private bool previousGroundCheck;
     private Vector2 facingDirection;
     private Vector3 groundOffset;
     private Vector3 wallOffset;
@@ -27,27 +28,33 @@ public class PlayerSurfaceDetection : MonoBehaviour
     {
         groundOffset = new Vector3(colliderOffset.x, 0, 0);
         wallOffset = new Vector3(0, colliderOffset.y, 0);
+        previousGroundCheck = onGround;
     }
 
     private void Update()
     {
         facingDirection = new Vector2(transform.localScale.x, 0);
-        //Determine if the player is stood on objects on the ground layer, using a pair of raycasts
-        onGround = Physics2D.Raycast(transform.position + groundOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - groundOffset, Vector2.down, groundLength, groundLayer);
-        onWall = Physics2D.Raycast(transform.position + wallOffset, facingDirection, wallLength, groundLayer) || Physics2D.Raycast(transform.position - wallOffset, facingDirection, wallLength, groundLayer);
     }
 
     private void FixedUpdate()
     {
-        if(onGround)
+        //Determine if the player is stood on objects on the ground layer, using a pair of raycasts
+        onGround = Physics2D.Raycast(transform.position + groundOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - groundOffset, Vector2.down, groundLength, groundLayer);
+        onWall = Physics2D.Raycast(transform.position + wallOffset, facingDirection, wallLength, groundLayer) || Physics2D.Raycast(transform.position - wallOffset, facingDirection, wallLength, groundLayer);
+
+        if (onGround != previousGroundCheck)
         {
-            playerCollider.size = colliderSizeGround;
-            playerCollider.edgeRadius = colliderRadiusGround;
-        }
-        else
-        {
-            playerCollider.size = colliderSizeAir;
-            playerCollider.edgeRadius = colliderRadiusAir;
+            previousGroundCheck = onGround;
+            if (onGround)
+            {
+                playerCollider.size = colliderSizeGround;
+                playerCollider.edgeRadius = colliderRadiusGround;
+            }
+            else
+            {
+                playerCollider.size = colliderSizeAir;
+                playerCollider.edgeRadius = colliderRadiusAir;
+            }
         }
     }
 
