99a5d1cb45745edd6b713885ead36bbc2a5b30ec
bruhenri12 <bruhenri12@hotmail.com>
2025-02-26 18:46:09 -0300

    Remove unused playerAxis logic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,7 +8,6 @@ namespace Brawlley
         #region Player Resources
         [Header("Player Resources")]
         [SerializeField] PlayerController playerController;
-        [SerializeField] GameObject playerAxis;
         #endregion
 
         #region Player Data
@@ -17,23 +16,6 @@ namespace Brawlley
         public string Team { get => team; set => team = value; }
 
         public GameInputs GameInputs => playerController.GameInputs;
-        #endregion
-
-        #region MonoBehaviour Lifecycle Methods
-        void Start()
-        {
-            if (playerAxis != null)
-                GameInputs.Player.Movement.performed += UpdateAxisDirection;
-        }
-        #endregion
-
-        #region Player Methods
-        public void UpdateAxisDirection(InputAction.CallbackContext context)
-        {
-            Vector2 movement = context.ReadValue<Vector2>();
-            if (movement.x != 0)
-                playerAxis.transform.rotation = Quaternion.Euler(0, movement.x > 0 ? 0 : 180, 0);
-        }
         #endregion
     }
 }
